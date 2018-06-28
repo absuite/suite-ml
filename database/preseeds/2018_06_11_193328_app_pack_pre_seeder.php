ab38b2216baa610c3fd4f43e663c9b3b717d64aa
suite <amiysd@163.com>
2018-06-28 23:23:43 +0800

    init

--- database/preseeds/2018_06_11_193328_app_pack_pre_seeder.php
+++ database/preseeds/2018_06_11_193328_app_pack_pre_seeder.php
@@ -1,13 +1,13 @@
 <?php
 
-use Gmf\Sys\Models\App\App;
+use Gmf\Sys\Models\Sv\Pack;
 use Illuminate\Database\Seeder;
 
 /**
  * 在数据库结构创建或者修改成功后，执行此逻辑，需要支持可多次重复执行.
  *
  */
-class AppAppPreSeeder extends Seeder
+class AppPackPreSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -16,7 +16,7 @@ class AppAppPreSeeder extends Seeder
      */
     public function run()
     {
-        App::BatchImport([
+        Pack::BatchImport([
 			['openid' => 'suite.cbo', 'code' => 'suite.cbo','name'=>'套件基础数据应用'],
 			['openid' => 'suite.amiba', 'code' => 'suite.amiba','name'=>'套件阿米巴应用'],
         ]);
