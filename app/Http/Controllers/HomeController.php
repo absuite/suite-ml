0ae19f2bd648b2471a6c770dc4cb0b7b0b48d9f5
gop <>
2018-08-27 13:41:03 +0800

    init

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,21 +11,26 @@ use Gmf\Sys\Models\User;
 use Illuminate\Http\Request;
 use Log;
 use GuzzleHttp;
-class HomeController extends Controller {
+
+class HomeController extends Controller
+{
   /**
    * Create a new controller instance.
    *
    * @return void
    */
-  public function __construct() {
+  public function __construct()
+  {
     //$this->middleware('visitor');
   }
-  public function demo(Request $request) {
+  public function demo(Request $request)
+  {
     $user = User::where('type', 'sys')->first();
     Auth::login($user);
     return redirect('/?demo=1');
   }
-  public function home(Request $request) {
+  public function home(Request $request)
+  {
     $isWx = false;
     if ($ua = $request->userAgent()) {
       $isWx = strpos(strtolower($ua), 'micromessenger') > 0;
@@ -41,29 +46,10 @@ class HomeController extends Controller {
   }
   public function testapi()
   {
-    $keysArr = array(
-      "id" => 8,
-      "optionId" => 732,
-    );
-    $client = new GuzzleHttp\Client([
-      'base_uri' => 'http://yungu.cying.com.cn',
-      'headers' => ['Accept' => 'application/json', 'Content-Type' => 'application/json'],
-      'verify' => false
-    ]);
-    $res = $client->request('POST', 'common/vote!ipcount.erun');
-    $result = (string)$res->getBody();
-    $result=json_decode($result);
-    if($result->msg){
-      throw new \Exception($result->msg);
-    }
-    
-    $res = $client->request('POST', 'common/vote!vote.erun', ['form_params' => $keysArr]);
-    $result = (string)$res->getBody();
-    $result=json_decode($result);
-    
-    return json_encode( $result);
+    return json_encode([]);
   }
-  public function getConfigs(Request $request) {
+  public function getConfigs(Request $request)
+  {
     $userId = GAuth::id();
     Log::error('HomeController:' . GAuth::id() . '--' . Auth::id());
     $config = new Builder;
