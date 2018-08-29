8a0e730c4ea9f17ce7b60839abbc1c80dc07b407
suite <amiysd@163.com>
2018-08-29 23:42:01 +0800

    init

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,27 +10,22 @@ use Gmf\Sys\Models as SysModels;
 use Gmf\Sys\Models\User;
 use Illuminate\Http\Request;
 use Log;
-use GuzzleHttp;
 
-class HomeController extends Controller
-{
+class HomeController extends Controller {
   /**
    * Create a new controller instance.
    *
    * @return void
    */
-  public function __construct()
-  {
+  public function __construct() {
     //$this->middleware('visitor');
   }
-  public function demo(Request $request)
-  {
+  public function demo(Request $request) {
     $user = User::where('type', 'sys')->first();
     Auth::login($user);
     return redirect('/?demo=1');
   }
-  public function home(Request $request)
-  {
+  public function home(Request $request) {
     $isWx = false;
     if ($ua = $request->userAgent()) {
       $isWx = strpos(strtolower($ua), 'micromessenger') > 0;
@@ -44,29 +39,28 @@ class HomeController extends Controller
     $config = new Builder;
     return view('app', ['jsWX' => $isWx]);
   }
-  public function testapi()
-  {
+  public function testapi() {
     return json_encode([]);
   }
-  public function getConfigs(Request $request)
-  {
+  public function getConfigs(Request $request) {
     $userId = GAuth::id();
     Log::error('HomeController:' . GAuth::id() . '--' . Auth::id());
     $config = new Builder;
     $config->user(GAuth::user());
     $entId = $request->session()->get(config('gmf.ent.session'));
     if (!empty($entId)) {
-      $entId = SysModels\Ent\EntUser::where('user_id', $userId)->where('ent_id', $entId)->value('ent_id');
+      $entId = SysModels\Ent\EntUser::where('user_id', $userId)->where('ent_id', $entId)->where('is_effective', '1')->where('revoked', '0')->value('ent_id');
     }
     if (empty($entId)) {
-      $entId = SysModels\Ent\EntUser::where('user_id', $userId)->orderBy('is_default', 'desc')->value('ent_id');
+      $entId = SysModels\Ent\EntUser::where('user_id', $userId)->where('is_effective', '1')->where('revoked', '0')->orderBy('is_default', 'desc')->value('ent_id');
     }
-    $ent = SysModels\Ent\Ent::find($entId);
-    if ($ent) {
-      $config->ent($ent);
-      session([config('gmf.ent.session') => $ent->id]);
+    if (!empty($entId)) {
+      $ent = SysModels\Ent\Ent::find($entId);
+      if ($ent) {
+        $config->ent($ent);
+        session([config('gmf.ent.session') => $ent->id]);
+      }
     }
-
     return $this->toJson($config);
   }
 
